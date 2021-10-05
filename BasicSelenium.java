ce1a75df760c80e4008e8e07c5eb757b01387cb0
ankitghorpade <75006687+ankitghorpade@users.noreply.github.com>
2021-10-05 12:45:31 +0530

    Update BasicSelenium.java

--- BasicSelenium.java
+++ BasicSelenium.java
@@ -9,12 +9,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class BasicSelenium {
 public static void main(String[] args)  {
 	//System.setProperty("key","value");
-	System.setProperty("webdriver.chrome.driver","/Users/ASUS/eclipse-workspace/seleniumDemo/driver/chromedriver.exe");
-	System.setProperty("webdriver.gecko.driver","/Users/ASUS/eclipse-workspace/seleniumDemo/driver/geckodriver.exe");
-	System.setProperty("webdriver.edge.driver","/Users/ASUS/eclipse-workspace/seleniumDemo/driver/msedgedriver.exe");
+	System.setProperty("webdriver.chrome.driver","/Users/ASUS/eclipse-workspace/seleniumDemo/driver1/chromedriver.exe");
+	System.setProperty("webdriver.gecko.driver","/Users/ASUS/eclipse-workspace/seleniumDemo/driver1/geckodriver.exe");
+	System.setProperty("webdriver.edge.driver","/Users/ASUS/eclipse-workspace/seleniumDemo/driver1/msedgedriver.exe");
 	WebDriver driver = new ChromeDriver();
-	WebDriver driver1 = new FirefoxDriver();
-	WebDriver driver2 = new EdgeDriver();
+// 	WebDriver driver1 = new FirefoxDriver();
+// 	WebDriver driver2 = new EdgeDriver();
 	
 	
 	driver.get("https://www.google.com/");
@@ -28,30 +28,30 @@ public static void main(String[] args)  {
 	driver.close();
 		driver.quit();
 	
-	driver1.get("https://www.google.com/");
-	try {
-		Thread.sleep(50000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	System.out.println(driver1.getTitle());
-	driver1.close();
-	  driver1.quit();
+// 	driver1.get("https://www.google.com/");
+// 	try {
+// 		Thread.sleep(50000);
+// 	} catch (InterruptedException e) {
+// 		// TODO Auto-generated catch block
+// 		e.printStackTrace();
+// 	}
+// 	System.out.println(driver1.getTitle());
+// 	driver1.close();
+// 	  driver1.quit();
 	  
-	driver2.get("https://www.flipkart.com/");
-	try {
-		Thread.sleep(80000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	System.out.println(driver2.getTitle());
-	System.out.println(driver2.getPageSource());
-	System.out.println(driver2.getCurrentUrl());
-	driver2.close();
-		driver2.quit();
+// 	driver2.get("https://www.flipkart.com/");
+// 	try {
+// 		Thread.sleep(80000);
+// 	} catch (InterruptedException e) {
+// 		// TODO Auto-generated catch block
+// 		e.printStackTrace();
+// 	}
+// 	System.out.println(driver2.getTitle());
+// 	System.out.println(driver2.getPageSource());
+// 	System.out.println(driver2.getCurrentUrl());
+// 	driver2.close();
+// 		driver2.quit();
 		
-}
+// }
 
 }
